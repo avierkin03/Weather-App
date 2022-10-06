2dacc97ba7715f60aaf6f3c9fe1ee916ae1672ac
avierkin03 <avierkin03@gmail.com>
2022-10-06 21:05:12 +0300

    Adding to weather-forecast JS template

--- index.js
+++ index.js
@@ -96,5 +96,28 @@ fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celcius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
+//Displaying the forecast for each day
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML = forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42" />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+      `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 //The first call of function searchCity()
 searchCity("New York");
+displayForecast();
